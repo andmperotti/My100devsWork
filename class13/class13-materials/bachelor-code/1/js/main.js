5e843916c64d4bb2bade9dcbe7ac4504436cee32
Andrew Perotti <perotti28@gmail.com>
2023-11-21 20:02:21 -0800

    Added work on class 13 js excercises; 3 bachelor related excercises

--- class13/class13-materials/bachelor-code/1/js/main.js
+++ class13/class13-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,17 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+// document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
 
-function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-}
+// function hide(){
+// 	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// 	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// }
+
+
+
+document.querySelector("#finalRose").addEventListener('click', ()=>{
+	document.querySelector('#claire').style.visibility = 'hidden'
+	document.querySelector('#sharleen').style.visibility = 'hidden'
+
+})
+
+//clair and sharleen go away
+//I did my code without clooking at his
--- class13/class13-materials/bachelor-code/3/js/main.js
+++ class13/class13-materials/bachelor-code/3/js/main.js
@@ -1,11 +1,27 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+const contestants = document.querySelectorAll(".contestant")
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
 
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains('rose')){
+		document.querySelector('#nikki').classList.toggle('hidden')
 	}else{
 		alert("Wrong!");
 	}
 }
+
+
+
+
+
+
+// document.querySelector(".rose").onclick = showNikki
+
+// function showNikki(){
+// 	document.querySelector("#nikki").classList.toggle('hidden')
+// }
+
+// document.querySelector('.contestant').onclick = wrong 
+// function wrong(){
+// 	alert('wrong')
+// }
