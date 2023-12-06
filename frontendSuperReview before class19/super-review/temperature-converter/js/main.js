81b2f533fa1b4225fea1b9f1c8b56566c89f9e37
Andrew Perotti <Perotti28@gmail.com>
2023-12-06 12:09:00 -0800

    Added work from the front end crash course review excercises

--- frontendSuperReview before class19/super-review/temperature-converter/js/main.js
+++ frontendSuperReview before class19/super-review/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
 //Write your pseduo code first! 
+//First add an event listener method that waits for a click
+document.querySelector('button').addEventListener('click', convertCel)
+//^pass it a function which here will:
+//create a variable to save user input 
+//target an element to output the converted value
+function convertCel(){
+    let cel = document.querySelector('input').value
+    document.querySelector('h1').innerText += ` ${cel*(9/5)+32}`
+}
--- frontendSuperReview before class19/super-review/variable-fun-time/js/main.js
+++ frontendSuperReview before class19/super-review/variable-fun-time/js/main.js
@@ -1,18 +1,21 @@
 //--- Easy
 //create a variable and assign it a number
-
+let age = 37
 //minus 10 from that number
-
+age -= 10
 //print that number to the console
-
+console.log(age)
 //--- Medium
 //create a variable that holds a value from the input
-
+let userInput = Number(document.querySelector("#danceDanceRevolution").value)
 //add 25 to that number
-
+userInput += 25
 //alert that number
-
+alert(userInput)
 //--- Hard
 //create a variable that holds the h1
-
+let hOne = document.querySelector('h1')
 //add an event listener to that element that console logs the sum of the two previous variables
+hOne.addEventListener('click', click=>{
+    console.log(age + Number(document.querySelector("#danceDanceRevolution").value))
+})
