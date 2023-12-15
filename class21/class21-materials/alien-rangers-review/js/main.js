9d37780830ca13714e0d99f9a4f94f61f4df5f4d
Andrew Perotti <Perotti28@gmail.com>
2023-12-15 12:17:24 -0800

    Added excercise work for class21 and added follow along materials for class22

--- class21/class21-materials/alien-rangers-review/js/main.js
+++ class21/class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,17 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
-
+let shows = ['brooklyn 99', 'community', 'parks and recreation']
+shows.forEach(e=>console.log(e))
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
-
+let nums = [28, 99, 69, 18]
+let evens = nums.filter(e=>e%2==0)
+console.log(evens)
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function doThing(arr){
+    let sortedArr = arr.sort((a,b)=>a-b)
+    alert(sortedArr[1]+sortedArr[sortedArr.length-2])
+}
+doThing([1,2,3,4,5])
--- class21/class21-materials/objects-zeo/js/main.js
+++ class21/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,24 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    shape: 'round', 
+    color: "black",
+    manufacturer: "nike", 
+    lanyard: true,
+    getTime : ()=>{
+        let now = new Date();
+        return `${now.getHours()}:${now.getMinutes()}`
+    },
+    displayTime: ()=>{
+        console.log(getTime())
+    },
+    startCounter: ()=>{
+        let now = new Date();
+        let startTime = `${now.getHours}:${now.getMinutes()}`
+    },
+    start: ()=>{
+        console.log('start')
+    },
+    stop: ()=>{
+        console.log("stop")
+    }
+}
