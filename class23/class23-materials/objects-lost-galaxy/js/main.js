72e25ccfda32ab35844fad2a8e08f8a42489b198
Andrew Perotti <Perotti28@gmail.com>
2023-12-26 11:31:18 -0800

    Added excercise work for class23

--- class23/class23-materials/objects-lost-galaxy/js/main.js
+++ class23/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,32 @@
 //Create a dog object that has four properties and three methods
+let rylee = {
+}
+rylee.name = "Rylee";
+rylee.age = 9;
+rylee.color = "brown";
+rylee.breed = "yorkshire terrier";
+rylee.wagTail = function(){
+    console.log(`${this.name} wags tail`)
+}
+rylee.licks = function(){
+    console.log(`${this.name} licks your hands`)
+}
+rylee.bark = function(){
+    console.log(`${this.name} is barking`)
+}
+
+function Dog(name, age, color, breed){
+    this.name = name,
+    this.age = age,
+    this.color = color, 
+    this.breed = breed,
+    this.wagTail = function(){
+        console.log(`$this.name} wags tail`)
+    },
+    this.licks = function(){
+        console.log(`${this.name} licks your hands`)
+    },
+    this.bark = function(){
+        console.log(`${this.name} is barking`)
+    }
+}
--- class23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,16 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that makes skating game characters with 4 properties and 3 methods
+function TonyHawkSkater(name, hairColor, skatingStyle, voiceLine){
+    this.name = name,
+    this.hairColor = hairColor,
+    this.skatingStyle = skatingStyle,
+    this.voiceLine = voiceLine,
+    this.describeSkater = function(){
+        console.log(`${this.name} has a hair color of ${this.hairColor}, and skates ${this.skatingStyle}`)
+    },
+    this.sayVoiceline = function(){
+        console.log(`${this.name} yells their voice line slogan, '${this.voiceLine}'.`)
+    },
+    this.doKickflip = function(){
+        console.log(`${this.name} does a kickflip`)
+    }
+}
