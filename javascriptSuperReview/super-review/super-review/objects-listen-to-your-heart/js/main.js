c41a8d09a7672423b7db5930cf1dba92a5df6b1d
Andrew Perotti <perotti28@gmail.com>
2024-02-27 19:09:52 -0800

    Added excercise work for js super review lecture

--- javascriptSuperReview/super-review/super-review/objects-listen-to-your-heart/js/main.js
+++ javascriptSuperReview/super-review/super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,14 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    brand: 'nike',
+    color: 'black and yellow',
+    display: 'black and white',
+    size: 'large', 
+    start(){console.log("Running!")},
+    stop(){console.log("Stop Now!")},
+    delay(){console.log("I'll excercise tomorrow.")}
+}
+console.log(stopwatch)
+stopwatch.start()
+stopwatch.stop()
+stopwatch.delay()
--- javascriptSuperReview/super-review/super-review/review-winter-games/js/main.js
+++ javascriptSuperReview/super-review/super-review/review-winter-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function doThing(arr){
+    let newArr = []
+    arr.forEach(e=>{
+        if(e%2==0){
+            newArr.push(e)
+        }
+    })
+    return newArr
+}
+console.log(doThing([1,2,3,4,5,6,7,8,9,10]))
+
