97ad66245ee1c27a12f8a345e536c5045595e9e6
Andrew Perotti <perotti28@gmail.com>
2024-03-04 19:14:45 -0800

    Added some howework to a pre existing file in the class 35 materials, and added follow along materials for class 36.

--- class35/class35-materials/node-intro-demo/demo.js
+++ class35/class35-materials/node-intro-demo/demo.js
@@ -1,9 +1,12 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http') //imports https module, which allows us to access the network
+const fs = require('fs') //imports file system module, which allows us to access files on the system
+
+//create a server by using the createServer method from the http module
 http.createServer((req, res) => {
+  //use the readfile method from the file system module passing in a callback function which we pass a specific html file that has elements and content, which is what our server will respond with to the devices that request from our server
   fs.readFile('demofile.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'}) //expect an html file
+    res.write(data) //sends the  files data to them
+    res.end() //tells the requester than the response is done
   })
-}).listen(8000)
+}).listen(8000) //this is the port this server is listening to for requests
