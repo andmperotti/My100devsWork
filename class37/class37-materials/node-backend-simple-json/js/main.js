b536f2c0f59f430c3699e133bdfc17384e28fb29
Andrew Perotti <Perotti28@gmail.com>
2024-03-08 12:21:06 -0800

    Added notes to server.js and index.js with what the lines of code were doing, as that's what the lecture was mainly about.

---
+++ class37/class37-materials/node-backend-simple-json/js/main.js
@@ -0,0 +1,15 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
+
+//So on the index.html when a user enters a value and clicks the button, invokes this function which saves their entry as 'userName' and then makes a fetch call which hits the /api on the server.js which builds an object depending on the value/query parameter and then returns an object, here we take that object and convert it to json and save as data and use that variable to log to the console and fill 3 elements with data using text.content
---
+++ class37/class37-materials/node-backend-simple-json/server.js
@@ -0,0 +1,93 @@
+//imports of modules so you can use methods from these modules
+const http = require('http');//network access
+const fs = require('fs')//file system access
+const url = require('url');//access urls, aka interfaces, aka buttons users use
+const querystring = require('querystring');//access query parameters on requests
+const figlet = require('figlet')//ascii art from strings
+
+//create a server by making a variable equal to the calling of createServer from the http module
+const server = http.createServer((req, res) => {
+  //page holds the pathname from a url, aka endpoint
+  const page = url.parse(req.url).pathname;
+  //params is the query parameter?
+  const params = querystring.parse(url.parse(req.url).query);
+  //log the page variable
+  console.log(page);
+
+  //if page requested endpoint == '/' then:
+  if (page == '/') {
+    //respond with index.html
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'}); //200 means ok
+      res.write(data);//populate response with data from index.html
+      res.end();//end the response
+    });//the only 2 things that really need to be changed per each below request is the endpoint and the file you're returning
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  //Here when a user enters text into input element and hits the button, the button invokes the makeReq function which calls this api using the input element value and responds with the below object 'objToJson'
+  else if (page == '/api') {
+    if('student' in params){
+      if(params['student']== 'leon'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "leon",
+          status: "Boss Man",
+          currentOccupation: "Baller"
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student = leon
+      else if(params['student'] != 'leon'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: "unknown",
+          status: "unknown",
+          currentOccupation: "unknown"
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student != leon
+    }//student if
+  }//else if, as in not api or any above endpoints:
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{ //if an endpoint is asked for but above rules don't supply it, serve a 404
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+//run this server on port 8000 aka localhost:8000
+server.listen(8000);
+
+//"page and params are deprecated now"-leon
+//page used pathname to find the endpoint
+//params looked at the query parameters 
+
+//
