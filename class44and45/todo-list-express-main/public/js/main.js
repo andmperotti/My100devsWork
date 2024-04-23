c85f29591ba99658931fa18f2466acfb3c80e423
Andrew Perotti <Perotti28@gmail.com>
2024-04-23 12:14:32 -0700

    Added comments to project files detailing what lines of code are doing in his node project example.

---
+++ class44and45/todo-list-express-main/public/js/main.js
@@ -0,0 +1,76 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+//the above variables are nodelists made up of all elements consisting of specific combinations of classes
+
+//here arrays are created from those nodelists and event listeners are invoked on each of those array items
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
+
+
+async function deleteItem(){
+    //this is the text content of the item which trash can was clicked, we'll use this text below to tell the server what to delete from the db
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ class44and45/todo-list-express-main/server.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+//this sets up the ejs as the view engine/templating tech
+app.set('view engine', 'ejs')
+//
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//This is triggered whenever the page is loaded or a refresh is needed which is typically after a post, or update, or delete
+app.get('/',async (request, response)=>{
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error))
+})
+
+//this is triggered when a user submits their inputted value via form element submit button, and we reference their input via request.body.todoItem which was the value of the name attribute for that input element.
+app.post('/addTodo', (request, response) => {
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    .then(result => {
+        console.log('Todo Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+//this is triggered when a user clicks on a rendered item 
+app.put('/markComplete', (request, response) => {
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+//this is triggered when a rendered item is clicked on
+app.put('/markUnComplete', (request, response) => {
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+//this is triggered when a user clicks the trash can icon next to an item
+app.delete('/deleteItem', (request, response) => {
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    .then(result => {
+        console.log('Todo Deleted')
+        response.json('Todo Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+//this sets up the port for the api to listen for requests on
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
